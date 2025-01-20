1a3c2b767f9c32285522a0d6418a46c8fd5481bf
Patricia <patricia.badji89@gmail.com>
2025-01-20 15:36:33 +0100

    Update POSTGRESQL.sql

--- POSTGRESQL.sql
+++ POSTGRESQL.sql
@@ -1,3 +1,12 @@
-SELECT nome 
-FROM clienti 
-WHERE nome LIKE '%Mario%'
+INSERT INTO Clienti (NumeroCliente, Nome, Cognome, Anno_di_Nascita, RegioneResidenza)
+VALUES
+(1, 'Luca', 'Rossi', 1985, 'Lazio'),
+(2, 'Mario', 'Bianchi', 1990, 'Toscana'),
+(3, 'Giovanni', 'Verdi', 1988, 'Emilia-Romagna'),
+(4, 'Francesca', 'Neri', 1992, 'Campania'),
+(5, 'Alessandro', 'Gialli', 1983, 'Lombardia'),
+(6, 'Sara', 'Sartori', 1991, 'Veneto'),
+(7, 'Davide', 'Mazzanti', 1995, 'Sicilia'),
+(8, 'Elena', 'Costa', 1987, 'Puglia'),
+(9, 'Marco', 'Marini', 1994, 'Piemonte'),
+(10, 'Giulia', 'Rinaldi', 1986, 'Liguria');
